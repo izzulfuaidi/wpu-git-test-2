1de665f83f53e346e303935c5fbcdf4809d4c796
Izzul Fuaidi <izzulfuaidi@gmail.com>
2023-08-29 15:46:06 +0700

    Menambahkan list

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+</head>
+<body>
+    <h1> hello world <h1>
+    <p> paragraf baru</p>
+    <li>satu</li>
+    <li>dua</li>
+</body>
+</html>
